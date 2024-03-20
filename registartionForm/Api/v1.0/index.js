b52f300534d83430dee871fcafd44cc431f55b38
DESKTOP-QVJLPFH\admn <n.sreenu@dev2prod.com>
2024-03-20 12:15:36 +0530

    index changes

--- registartionForm/Api/v1.0/index.js
+++ registartionForm/Api/v1.0/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/createRegistration", require("./createUser"));
+router.use("/createUser", require("./createUser"));
 router.use("/getUsers",require("./getUsers"));
 router.use("/getUsersById",require("./getUserById"));
 router.use("/deleteUser",require("./deleteUser"));
